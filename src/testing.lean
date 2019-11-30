bfc7e0e931ac3ead90f5b96f2ee03943c0fb2521
David Renshaw <dwrenshaw@gmail.com>
2019-11-30 16:16:25 -0500

    more from the tutorial

--- src/testing.lean
+++ src/testing.lean
@@ -13,3 +13,14 @@ def print_squares : ℕ → io unit
                 put_str (to_string n ++ "^2 = " ++ to_string (n * n) ++ "\n")
 
 #eval print_squares 100
+
+meta def m91 : ℕ -> ℕ
+| n := if n > 100 then n - 10 else m91 (m91 (n + 11))
+
+#eval m91 10
+#eval m91 100
+
+meta def foo: ℕ -> ℕ
+| n:= foo n + 1
+
+#reduce foo
