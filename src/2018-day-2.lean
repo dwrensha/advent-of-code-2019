5e2317b70d2db45085822d45400538b4c3acb190
David Renshaw <dwrenshaw@gmail.com>
2019-11-30 21:33:53 -0500

    part 2

--- src/2018-day-2.lean
+++ src/2018-day-2.lean
@@ -9,7 +9,7 @@ do s ← io.fs.read_file file,
 
 def parse_words := sep_by (do _ ← sat char.is_whitespace, return ()) (many (sat char.is_alpha))
 
-#check parse_words
+def day_2 := go "src/2018-day-2-input.txt" parse_words
 
 def helper2 : char → ℕ → bool → bool
 | _ _ tt := tt
@@ -41,6 +41,29 @@ def iter : list (list char) → ℕ → ℕ → option string
      let thrice' := if has_same_char_thrice t then thrice + 1 else thrice in
      iter ws twice' thrice'
 
-#eval  go "src/2018-day-2-input.txt" parse_words $ λ l, iter l 0 0 -- 5928
+#eval day_2 $ λ l, iter l 0 0 -- 5928
 
 
+def word_diff : list char → list char → ℕ → ℕ
+| [] [] n := n
+| [] (c::cs) _ := 100 -- arbitrary number larger than 1
+| (c::cs) [] _ := 100 -- arbitrary number larger than 1
+| (c::cs) (d::ds) n :=
+  let inc := if c = d then 0 else 1 in
+  word_diff cs ds (n + inc)
+
+def common_letters : list char → list char → list char
+| [] [] := []
+| (c::cs) (d::ds) :=
+  if c = d then c::(common_letters cs ds)
+  else common_letters cs ds
+| _ _ := [] -- would be better to indicate failure
+
+meta def iter2: list (list char) → list (list char) → option string
+| [] [] := none
+| [] (v::vs) := iter2 vs vs
+| (w::ws) (v::vs) := if word_diff w v 0 = 1 then some (common_letters w v).as_string
+                     else iter2 ws (v::vs)
+| _ _ := none
+
+#eval day_2 $ λ l, iter2 l l
