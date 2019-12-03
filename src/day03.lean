1a3c869608aa54a159dbe18b8851bb9a48a50897
David Renshaw <dwrenshaw@gmail.com>
2019-12-03 18:17:16 -0500

    more parsing

--- src/day03.lean
+++ src/day03.lean
@@ -20,6 +20,7 @@ def parse_step : parser step :=
   do
     c ← one_of ['R', 'L', 'U', 'D'],
     n ← parse_number,
+    trace ((to_string c) ++ (to_string n)) (return ()),
     return (match c with | 'R' := step.right n
                          | 'L' := step.left n
                          | 'U' := step.up n
@@ -27,6 +28,13 @@ def parse_step : parser step :=
                          | _ := sorry
             end)
 
+def parse_wires : parser (list step × list step) :=
+  do
+   wire1 ← sep_by (ch ',') parse_step,
+   ch '\n',
+   wire2 ← sep_by (ch ',') parse_step,
+   return ⟨wire1, wire2⟩
 
+def day03 := go "day03.txt" parse_wires
 
---def day03 := go "day03.txt" ((many (parse_number <* ch ',')) <* ch '\n')
+#eval day03 $ λ _, some "SUCCESS"
