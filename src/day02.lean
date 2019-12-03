dfc6abca0e1b84aa50a536999c615577dc6ca7f2
David Renshaw <dwrenshaw@gmail.com>
2019-12-02 20:37:00 -0500

    part 1

--- src/day02.lean
+++ src/day02.lean
@@ -9,27 +9,42 @@ do s ← io.fs.read_file file,
 
 def parse_number := string.to_nat <$> many_char1 (sat char.is_digit)
 
-def day1 := go "day02.txt" (many (parse_number <* ch ','))
+def day02 := go "day02.txt" ((many (parse_number <* ch ',')) <* ch '\n')
 
-lemma foo {n m : ℕ} (h: n ≤ m) (x : fin n) : x.val < m :=
-let hx: x.val < n := x.is_lt in
-lt_of_lt_of_le hx h
+def nth : list ℕ → ℕ → ℕ
+| [] _ := sorry
+| (c::cs) 0 := c
+| (c::cs) (succ idx) := nth cs idx
 
-lemma le_plus_four {n : ℕ} : n ≤ n + 4 :=
-less_than_or_equal.step $ less_than_or_equal.step $ less_than_or_equal.step $ less_than_or_equal.step $ less_than_or_equal.refl n
+def replace_nth : list ℕ → ℕ → ℕ → list ℕ
+| [] _ _ := sorry
+| (c::cs) 0 new_value := new_value::cs
+|  (c::cs) (succ idx) new_value := c::(replace_nth cs idx new_value)
 
-def coerce_fin {n m : ℕ} : ( n ≤ m ) → fin n → fin m
-| hleq x := ⟨ x.val, foo hleq x ⟩
-
--- map current memory, current pointer, to new memory, and flag for done or not
-def step (s: ℕ) : (array (s + 4) ℕ) → fin s → ((array (s + 4) ℕ) × bool)
+-- map current memory, current pointer, to ending memory
+meta def execute : list ℕ → ℕ → list ℕ
 | m idx :=
-let current := m.read (coerce_fin le_plus_four idx)  in
-if current = 99 then ⟨ m, true ⟩
-else if current = 1 then sorry
-else sorry
+let x := nth m idx in
+if x = 99 then m
+else
+  let arg1 := nth m (idx + 1) in
+  let arg2 := nth m (idx + 2) in
+  let arg3 := nth m (idx + 3) in
+  let inp1 := nth m arg1 in
+  let inp2 := nth m arg2 in
+  let new_value := if x = 1 then inp1 + inp2
+                   else if x = 2 then inp1 * inp2
+                   else sorry in
+  let new_list := replace_nth m arg3 new_value in
+  execute new_list (idx + 4)
+
+#eval day02 $ λ l,
+  let l' := replace_nth l 1 12 in
+  let l'' := replace_nth l' 2 2 in
+  let final := execute l'' 0 in
+  to_string (nth final 0)
 
 
-#check (⟨λ _, 0⟩: array 60 ℕ)
-
+#eval day02 $ λ l,
+  some (list.foldl (λ a: string, λb : ℕ, a ++ (to_string b) ++ ",") "" l)
 
